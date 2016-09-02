e7afe599724a557e58866d5919f6f57d96ac4e68
Round Dumpling <round.dumpling@gmail.com>
2016-09-01 21:56:19 -0700

    code cleanup

--- install/roles/docker/templates/config-cbr0.sh
+++ install/roles/docker/templates/config-cbr0.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-netaddr="$1"
-if [ -z "$1" ]
-then
-    # If there is no subnet provided, we configure cbr0 on 10.2.0.0 network if it doesn't exist
-    cbr0=`ifconfig -a | grep cbr0`
-    if [ -n "$cbr0" ]
-    then
-        exit 0
-    fi
+flanneladdr=`ifconfig flannel.1 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1`
+flannelmtu=`ifconfig flannel.1 | grep MTU | cut -d ":" -f 2 | cut -d " " -f 1`
 
-    netaddr="{{ flannel_subnet }}"
-fi
-
+echo "configuring cbr0 to use ${flanneladdr} mtu ${flannelmtu}"
 # remove the existing docker0 and cbr0 setup
 iptables -t nat -F
 ip link set docker0 down
@@ -24,7 +15,13 @@ rm -rf /var/lib/docker/network
 
 # add cbr0
 brctl addbr cbr0
-ifconfig cbr0 "$netaddr" up
+ifconfig cbr0 ${flanneladdr} up
 ifconfig cbr0 netmask 255.255.255.0 broadcast 0.0.0.0
 ip link set dev cbr0 up
-iptables -t nat -A POSTROUTING ! -d "$netaddr" -m addrtype ! --dst-type LOCAL -j MASQUERADE
+iptables -t nat -A POSTROUTING ! -d "$flanneladdr" -m addrtype ! --dst-type LOCAL -j MASQUERADE
+
+if [ -z "$flannelmtu" ]
+then
+    flannelmtu=1450
+fi
+echo {\"mtu\": ${flannelmtu}} > /opt/docker/config.json
